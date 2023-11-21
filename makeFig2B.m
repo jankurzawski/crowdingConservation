b1d996da4ff97aaf914f01afe14c945d905e5b50
Jan Kurzawski <jk7127@deathvalley.psych.nyu.edu>
2023-11-21 12:43:21 -0500

    add radius at 10 deg

--- makeFig2B.m
+++ makeFig2B.m
@@ -20,7 +20,20 @@ boumas = [bouma(S_low_bouma) bouma(S_high_bouma)];
 % eccentricity = 1
 
 for b = 1 : length(boumas)
-    
     crowding_Visualize_Letters(boumas(b),2,0.24,10,0,1);
     axis off
+    plotrings(10)
+    hgexport(gcf, sprintf('./figures/letter_plot_subject%i.eps',b));
+
+
+end
+
+
+function [h] = plotrings(r)
+
+    
+
+    th = 0:pi/50:2*pi;
+    h =polarplot(th,r+zeros(size(th)),'Color',[0 0 0 0.15],'LineStyle',':','LineWidth',2);
+
 end
