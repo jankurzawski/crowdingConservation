ff29a5077fcdec8c56364fad36f1bed9472a138d
Jan Kurzawski <jan.kurzawski@gmail.com>
2023-12-05 12:11:32 -0500

    update description of load surf

--- code/load_from_raw.m
+++ code/load_from_raw.m
@@ -4,7 +4,7 @@ function [bouma, area] = load_from_raw(surfaceType)
 % this function prepares data for analysis and plotting. It loads the
 % datatable created by load_crowding and creates one threshold per observer
 % Using load_surface it calculates surface area of visual maps from a
-% specified surface and averages across hemispheres.
+% specified surface (white, midgray, pial) and averages across hemispheres.
 
 [datatable] = load_crowding('./data/crowdingData');
 u_ses       = unique(datatable.Session);
