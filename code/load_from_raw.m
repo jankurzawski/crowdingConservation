c1be63e9cef92debbafa059ad8ca7c7e015f406b
Jan Kurzawski <jan.kurzawski@gmail.com>
2023-12-06 10:37:03 -0500

    update comments

--- code/load_from_raw.m
+++ code/load_from_raw.m
@@ -4,7 +4,7 @@ function [bouma, area] = load_from_raw(surfaceType)
 % this function prepares data for analysis and plotting. It loads the
 % datatable created by load_crowding and creates one threshold per observer
 % Using load_surface it calculates surface area of visual maps from a
-% specified surface (white, midgray, pial) and averages across hemispheres.
+% specified surface (white, midgray, pial) and sums across hemispheres.
 
 [datatable] = load_crowding('./data/crowdingData');
 u_ses       = unique(datatable.Session);
@@ -32,7 +32,9 @@ bouma       = mean(bouma_sess);
 if ~exist('surfaceType', 'var') || isempty(surfaceType), surfaceType = 'midgray'; end
 
 hemi        = {'lh';'rh'};
+% load ROIs from researcher 1
 researcher1 = load_surface('./data/surfaceData',surfaceType,'R1',hemi);
+% load ROIs from researcher 2
 researcher2 = load_surface('./data/surfaceData',surfaceType,'R2',hemi);
 
 r1          = sum(researcher1, 3);   % sum across hemispheres
