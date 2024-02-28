b8d5604306cbc74fa54000db183fce4fcdb8f8bf
Jan Kurzawski <jan.kurzawski@gmail.com>
2024-02-28 09:59:47 -0500

    calculate sd for two example observers

---
+++ estimate_sd_exmaple_observer.m
@@ -0,0 +1,27 @@
+clc
+clear
+close all
+[directory,~] = fileparts(mfilename('fullpath'));
+cd(directory);
+addpath(genpath('data'))
+addpath(genpath('code'))
+addpath(genpath('extra'))
+
+load subjects_ID
+[bouma, area] = load_from_raw([],3,[]);
+
+
+S_low_bouma = strcmp('sub-wlsubj045',subjects);
+S_high_bouma = strcmp('sub-wlsubj117',subjects);
+boumas = [bouma(:,S_low_bouma,:) bouma(:,S_high_bouma,:)];
+
+subj_mean = NaN(1,2);
+subj_sd    = NaN(1,2);
+
+for s = 1 : size(subj_mean,2);
+    
+    subj_data = squeeze(boumas(:,s,:));
+    subj_mean(s) = geomean(geomean(subj_data))
+    subj_sd(s) = mean(std(subj_data));
+    
+end
