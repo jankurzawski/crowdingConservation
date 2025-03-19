b31091b9604b7d15f25ad8e0af4e5e7d21a32f77
jankurzawski <administrator@FDPMAC066.local>
2025-03-19 21:34:16 +0100

    fixing script names

--- makeFig2B.m
+++ makeFig2B.m
@@ -26,32 +26,9 @@ for b = 1 : length(boumas)
     
     crowding_visualize_Letters(boumas(b),2,0.24,10,0,1,'r',cmap(b,:));
     axis off
-    addlines(5);
     
 end
 
+polarplot([deg2rad(0) deg2rad(0)],[-0.5 0.5],'Color',[0 0 0],'LineStyle','-','LineWidth',2)
+polarplot([deg2rad(90) deg2rad(90)],[-0.5 0.5],'Color',[0 0 0],'LineStyle','-','LineWidth',2)
 
-function [h] = addlines(r)
-
-    % add eccentricity ring
-    th = 0:pi/50:2*pi;
-    h = polarplot(th,r+zeros(size(th)),'Color',[0 0 0 0.15],'LineStyle',':','LineWidth',2);
-    
-    
-    xc = 3.5;
-    yc = 0;
-    
-    [x,y] = pol2cart(th,2);
-    [th1, r1] = cart2pol(x+xc, y+yc);
-%     h = polarplot(th,r+zeros(size(th)),'Color',[0 0 0 0.15],'LineStyle',':','LineWidth',2);
-%     h = polarplot(th1,r1+zeros(size(th)),'Color',[0 0 0 0.15],'LineStyle',':','LineWidth',2);
-% 
-%     % add wedge outline
-%     polarplot([deg2rad(45) deg2rad(45)],[0 10],'Color',[0 0 0 0.15],'LineStyle',':','LineWidth',2)
-%     polarplot([deg2rad(-45) deg2rad(-45)],[0 10],'Color',[0 0 0 0.15],'LineStyle',':','LineWidth',2)
-%     
-    % add fixation
-    polarplot([deg2rad(0) deg2rad(0)],[-0.5 0.5],'Color',[0 0 0],'LineStyle','-','LineWidth',2)
-    polarplot([deg2rad(90) deg2rad(90)],[-0.5 0.5],'Color',[0 0 0],'LineStyle','-','LineWidth',2)
-    
-end
